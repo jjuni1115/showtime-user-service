4f466290c66586061abd85856cb60a2fcb202db6
AuthorName <junyege@naver.com>
2025-03-06 17:34:51 +0900

    feat:Enhance OAuth2 success handler and JWT token generation with user details

--- src/main/java/com/shwotime/userservice/handler/CustomOAuthSuccessHandler.java
+++ src/main/java/com/shwotime/userservice/handler/CustomOAuthSuccessHandler.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
@@ -33,11 +34,17 @@ public class CustomOAuthSuccessHandler implements AuthenticationSuccessHandler {
         if(role!=null && role.equals("ROLE_GUEST")){
 
             //TODO: redirect to registration page
+            response.sendRedirect("http://localhost:5173/signup");
 
         }else{
 
             //TODO: generate token and redirect to home page
-            jwtTokenProvider.generateToken(email);
+            String token = jwtTokenProvider.generateToken(email);
+
+            String redirectUrl = UriComponentsBuilder.fromUriString("http://localhost:5173/home").queryParam("token",token).build().toUriString();
+
+            response.sendRedirect(redirectUrl);
+
 
 
         }
--- src/main/java/com/shwotime/userservice/util/JwtTokenProvider.java
+++ src/main/java/com/shwotime/userservice/util/JwtTokenProvider.java
@@ -1,12 +1,17 @@
 package com.shwotime.userservice.util;
 
+import com.shwotime.userservice.entity.UserEntity;
 import com.shwotime.userservice.repository.UserRepository;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
@@ -15,28 +20,43 @@ public class JwtTokenProvider {
 
     private final UserRepository userRepository;
 
-    private final Key key;
+    private final SecretKey key;
     private final long expirationTime;
 
     public JwtTokenProvider(UserRepository userRepository, @Value("${spring.jwt.secret}") String secret, @Value("${spring.jwt.expiration}") long expirationTime) {
         this.userRepository = userRepository;
-        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         this.expirationTime = expirationTime;
     }
 
-    public String generateToken(String userEmail){
+    public String generateToken(String userEmail) {
+
+        UserEntity user = userRepository.findByEmail(userEmail).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+
         return Jwts.builder()
                 .subject(userEmail)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .claim("userEmail", user.getEmail())
+                .claim("userName", user.getName())
+                .claim("userId", user.getId())
                 .signWith(key)
                 .compact();
     }
 
-    //TODO: generate refresh token and validate token methods
+    //TODO: generate refresh token
 
 
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
 
+    }
 
 
 }
